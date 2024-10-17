da1dd220bf75b552511164de58d8dae18b2fbbc4
RabiulMannanSajed <jahiduddin1389@gmail.com>
2024-10-17 22:41:49 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+// const express = require("express");
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  var a = 10;
+  res.send(a);
+});
+
+console.log(process.cwd());
+export default app;
