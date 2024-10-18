7e9ba0234ae5b3bf53e2cf1ea6aa6570f69af73b
RabiulMannanSajed <rabiulmannansajed@gmail.com>
2024-10-18 22:20:37 +0600

    adde files

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,32 @@
+import { Schema, model, connect } from 'mongoose';
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+// * this is the main
+export type Student = {
+  id: string;
+  name: UserName;
+  //type union letaral
+  gender: 'male' | 'female';
+  email: string;
+  avatar?: string;
+  dataOfBirth: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+};
